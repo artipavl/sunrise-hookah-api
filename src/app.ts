06ba0f6d412ff03b9b0d9dfe0d100e2f5fc1cc94
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-12 21:12:52 +0300

    add firebase-admin

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,37 @@
+// import admin from "firebase-admin";
 import express from "express";
+import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import admin from "./server";
+
 dotenv.config();
 
 const app = express();
 
 // app.use(helmet());
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (_, res) => {
+app.post("/", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const auth = await admin.auth().createUser({
+      email,
+      password,
+      emailVerified: false,
+      disabled: false,
+    });
+    console.log(auth);
+  } catch (error) {
+    console.log(error);
+  }
+
   res.send("Hello World!");
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,39 @@
 import dotenv from "dotenv";
-
+import admin from "firebase-admin";
 dotenv.config();
 
 import app from "./app";
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, function () {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
+
+async function start() {
+  try {
+    await admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      databaseURL:
+        "https://sunrise-hookah-default-rtdb.europe-west1.firebasedatabase.app",
+    });
+    await app.listen(PORT, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
+
+export default admin;
