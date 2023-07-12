37cd832b7fa58c1aa9682bc8c33a032b115009f8
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-12 22:08:39 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (_, res) => {
+  res.send("Oooooo");
+});
+
 app.post("/", async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -27,11 +31,11 @@ app.post("/", async (req, res) => {
       disabled: false,
     });
     console.log(auth);
+    res.json({ email: auth.email });
   } catch (error) {
     console.log(error);
+    res.json("error");
   }
-
-  res.send("Hello World!");
 });
 
 module.exports = app;
