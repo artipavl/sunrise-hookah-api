536dbefdba67035535c7658faefb0f8eceb28832
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-14 19:37:20 +0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,34 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { initializeApp, cert } from "firebase-admin/app";
+import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import {
   getFirestore,
   // Timestamp,
   // FieldValue,
   // Filter,
 } from "firebase-admin/firestore";
-
+import serviceAccount from "./secrets/serviceAccount.json";
 import app from "./app";
+
 const PORT = process.env.PORT;
 
-const serviceAccount = JSON.parse(
-  JSON.stringify({
-    type: process.env.type,
-    project_id: process.env.project_id,
-    private_key: process.env.private_key,
-    client_email: process.env.client_email,
-    client_id: process.env.client_id,
-    auth_uri: process.env.auth_uri,
-    token_uri: process.env.token_uri,
-    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-    client_x509_cert_url: process.env.client_x509_cert_url,
-    universe_domain: process.env.universe_domain,
-  })
-);
+// const serviceAccount = JSON.parse(
+//   JSON.stringify({
+//     type: process.env.type,
+//     project_id: process.env.project_id,
+//     private_key: process.env.private_key,
+//     client_email: process.env.client_email,
+//     client_id: process.env.client_id,
+//     auth_uri: process.env.auth_uri,
+//     token_uri: process.env.token_uri,
+//     auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+//     client_x509_cert_url: process.env.client_x509_cert_url,
+//     universe_domain: process.env.universe_domain,
+//   })
+// );
 
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert(serviceAccount as ServiceAccount),
   databaseURL:
     "https://sunrise-hookah-default-rtdb.europe-west1.firebasedatabase.app",
 });
