c068add5034e41d2fb9309bcf180bb8e17a47778
Kindual <110603179+Kindual@users.noreply.github.com>
2023-07-17 23:25:09 +0300

    add Auth

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,11 @@
 import ctrlWrapper from "./CtrlWrapper";
 import HttpError from "./HttpError";
 import isValidBody from "./isValidBody";
+import Authorization from "./Authorization";
 
 export {
     ctrlWrapper,
     HttpError,
     isValidBody,
+    Authorization,
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import admin from "firebase-admin";
+import { initializeApp } from 'firebase/app';
+
 import fs from "fs";
 dotenv.config();
 
@@ -24,21 +26,23 @@ const PORT = process.env.PORT;
 async function start() {
   try {
     const sourceFilePath = "/etc/secrets/serviceAccount.json";
+    const sourceFilePathAuth = "/etc/secrets/firebaseConfig.json";
     const test = await fs.promises.readFile(sourceFilePath, "utf8");
+    const testAuth = await fs.promises.readFile(sourceFilePathAuth, "utf8");
     const serviceAccount = JSON.parse(test);
-    if (!serviceAccount) {
+    const firebaseConfig = JSON.parse(testAuth);
+    if (!serviceAccount || !firebaseConfig) {
       console.log("serviceAccount === null");
       return;
     }
-
-    serviceAccount &&
-      (await admin.initializeApp({
-        credential: admin.credential.cert(
-          serviceAccount as admin.ServiceAccount
-        ),
-        databaseURL:
-          "https://sunrise-hookah-default-rtdb.europe-west1.firebasedatabase.app",
-      }));
+    await initializeApp(firebaseConfig)
+    await admin.initializeApp({
+      credential: admin.credential.cert(
+        serviceAccount as admin.ServiceAccount
+      ),
+      databaseURL:
+        "https://sunrise-hookah-default-rtdb.europe-west1.firebasedatabase.app",
+    });
 
     await app.listen(PORT, function () {
       console.log(`Server running. Use our API on port: ${PORT}`);
