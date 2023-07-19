fa942bd81c3018da3acb3d3ef11b7fc1f5fdb4a7
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-19 14:59:41 +0300

    add getAllType

--- src/controllers/type/index.ts
+++ src/controllers/type/index.ts
@@ -2,5 +2,6 @@ import { addType } from "./addType";
 import { getTypeById } from "./getTypeById";
 import { removeType } from "./removeType";
 import { updateType } from "./updateType";
+import { getAllType } from "./getAllType";
 
-export { addType, getTypeById, removeType, updateType };
+export { addType, getTypeById, removeType, updateType, getAllType };
