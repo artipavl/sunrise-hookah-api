f4d7e8a218d154c5513292f4a25951eeee1c3835
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-19 14:23:33 +0300

    Update getTovarByid.ts

--- src/controllers/tovar/getTovarByid.ts
+++ src/controllers/tovar/getTovarByid.ts
@@ -4,21 +4,12 @@ import { ctrlWrapper } from "../../helpers";
 
 export const getTovarByid = ctrlWrapper(async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { p = "10", uid = "" } = req.query;
 
-  const Type = await admin
-    .firestore()
-    .collection("tovars")
-    .orderBy("type")
-    .where("type", "==", id)
-    .startAfter(uid)
-    .limit(Number(p))
-    .count()
-    .get();
+  const Type = await admin.firestore().collection("tovars").doc(id).get();
 
   if (!Type.data()) {
     res.status(400).json("Bad Request");
   }
 
-  res.json({ ...Type.data() });
+  res.json({ ...Type.data(), id: Type.id });
 });
