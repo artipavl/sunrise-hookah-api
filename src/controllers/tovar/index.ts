62d77042041653a7a1987467d3485b1e3a0859e1
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-19 15:31:39 +0300

    Update index.ts

--- src/controllers/tovar/index.ts
+++ src/controllers/tovar/index.ts
@@ -2,5 +2,6 @@ import { getTovarByType } from "./getTovarByType";
 import { getTovarByid } from "./getTovarByid";
 import { addTovar } from "./addTovar";
 import { removeTovar } from "./removeTovar";
+import { updateTovarById } from "./updateTovarById";
 
-export { getTovarByType, getTovarByid, addTovar, removeTovar };
+export { getTovarByType, getTovarByid, addTovar, removeTovar, updateTovarById };
