37b809e41123e9dccd8d431231a208a3bc67d95c
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-19 15:31:48 +0300

    add updateTovarById

---
+++ src/controllers/tovar/updateTovarById.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import { ctrlWrapper, uploadImageToStorage } from "../../helpers";
+import admin from "firebase-admin";
+import { TovarType } from "../../Types";
+import { unlinkSync } from "fs";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
+export const updateTovarById = ctrlWrapper(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const files = req.files;
+    const tovar: TovarType = req.body;
+
+    const get = await admin.firestore().collection("tovars").doc(id).get();
+
+    if (!get.data()) {
+      res.status(400).json("Bad Request");
+      return;
+    }
+
+    const newtovat = await admin
+      .firestore()
+      .collection("tovars")
+      .doc(id)
+      .update({ ...tovar });
+
+    console.log(newtovat);
+
+    const bucket = admin.storage().bucket();
+
+    const fotos: string[] = [];
+    const storage = getStorage();
+
+    if (files && files instanceof Array) {
+      for (let index = 0; index < files.length; index++) {
+        await uploadImageToStorage(
+          bucket,
+          files[index],
+          `tovars/${id}/${files[index].originalname}`
+        );
+        fotos.push(
+          await getDownloadURL(
+            ref(storage, `tovars/${id}/${files[index].originalname}`)
+          )
+        );
+        unlinkSync(files[index].path);
+      }
+    }
+
+    await admin.firestore().collection("tovars").doc(id).update({ fotos });
+
+    res.status(201).json({ ...tovar, fotos, id });
+  }
+);
