54dfb54c6a8a0ae532b12b8919a76cbe0c329ab9
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-05 13:37:46 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,21 +7,9 @@ dotenv.config();
 
 import app from "./app";
 
-// import serviceAccount from "./serviceAccount.json";
 const PORT = process.env.PORT;
 
-// const serviceAccount = {
-//   type: process.env.type,
-//   project_id: process.env.project_id,
-//   private_key: process.env.private_key,
-//   client_email: process.env.client_email,
-//   client_id: process.env.client_id,
-//   auth_uri: process.env.auth_uri,
-//   token_uri: process.env.token_uri,
-//   auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-//   client_x509_cert_url: process.env.client_x509_cert_url,
-//   universe_domain: process.env.universe_domain,
-// };
+
 
 async function start() {
   try {
